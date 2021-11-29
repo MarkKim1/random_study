375dd1f66b994eebfcdb91ae03cd7691451eb62d
Minseok Kim <kim970602@gmail.com>
2021-11-29 17:26:40 +0900

    some studies

---
+++ deque.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <deque>
+
+using namespace std;
+
+int main () {
+    int testCase;
+    string command,member;
+    cin >> testCase;
+
+    for(int i = 0; i < testCase; i++){
+        cin >> command;
+        if(command == "push_back"){
+            cin >> member;
+            
+        }
+    }
+}
