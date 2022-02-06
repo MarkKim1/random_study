214917ff220ea82fd92c71980c22a7cdcd7d0217
Minseok Kim <kim970602@gmail.com>
2022-02-06 22:32:04 +0900

    hash

--- phonebook.cpp
+++ phonebook.cpp
@@ -9,11 +9,7 @@ int main (){
    vector<string> phonebook;
    string num;
    string list;
-   int number = 0;
-   int count = 0;
-   int vector = 1;
-   int flag = -1;
-   int other_flag = 0;
+   bool answer = true;
    getline(cin,list);
    for(int i= 0; i < list.size();i++){
       if(isnumber(list.at(i))){
@@ -29,29 +25,13 @@ int main (){
       }
    }
    sort(phonebook.begin(),phonebook.end());
-   number = stoi(phonebook.at(0));
-   while(number > 1){
-      number /=10;
-      count++;
-   }
-   int other_count = count;
-   int other_vector = 0;
-   while(true){
-      if(phonebook[other_vector++][other_count] == phonebook[0][other_count]){
-         if(phonebook[other_vector][other_count] == phonebook[0][other_count]){
-         flag++;
-         other_count--;
-            if(flag == count){
-               cout << "false" << endl;
-               break;
-               flag = 1;
-            }
-         }
+   for(int i = 0; i < phonebook.size()-1; i++){
+      if(phonebook.at(i) == phonebook.at(i+1).substr(0,phonebook.at(i).size())){
+         answer = false;
+         break;
       }
-   }
-   if(flag == 0){
-         cout << true << endl;
-
-   }                   
+   }       
+   cout << boolalpha;
+   cout << answer<<endl;
    return 0; 
 }
