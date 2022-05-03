65993195e89796f508ae106e04d42889268e8e69
Minseok Kim <kim970602@gmail.com>
2022-05-03 23:23:38 +0900

    handled methods

--- id_recomend.cpp
+++ id_recomend.cpp
@@ -1,48 +1,73 @@
-#include <iostream>
-#include <string>
-#include <vector>
+    #include <iostream>
+    #include <string>
+    #include <vector>
 
-using namespace std;
+    using namespace std;
 
-string solution(string new_id) {
-    string answer = "";
-    if(new_id == ""){
-        new_id +='a';
-    }
-    //step 1
-    for(int i = 0; i < new_id.size(); i++){
-        if(isalpha(new_id[i])){
-            new_id[i] = tolower(new_id[i]);
+    string ifempty(string new_id){
+        if(new_id == ""){
+            for(int i = 0; i < 3; i++){
+                new_id+='a';
+            }
         }
-    }
-    //step2, step3 
-    char temp = '\0';
-    for(int i = 0; i < new_id.size(); i++){
-        if(isalpha(new_id[i]) || isdigit(new_id[i]) || 
-           new_id[i] == '-' || new_id[i] == '_' || new_id[i] == '.'){
-               if(temp == '.' && new_id[i] == '.')
-               continue;
-               
-               answer+=new_id[i];
-           }
-           temp = new_id[i];
-    }
-    //step5
-    
-
-    //step 4
-    if(answer[0] == '.'){
-        answer.erase(answer.begin() + 0);
-    }
-    int size = answer.size()-1;
-    if(answer[size] == '.'){
-        answer.erase(answer.begin() + size);
+        return new_id;
     }
 
+    string solution(string new_id) {
+        string answer = "";
+        new_id = ifempty(new_id);
+        //step 1
+        for(int i = 0; i < new_id.size(); i++){
+            if(isalpha(new_id[i])){
+                new_id[i] = tolower(new_id[i]);
+            }
+        }
+        //step2, step3 
+        char temp = '\0';
+        for(int i = 0; i < new_id.size(); i++){
+            if(isalpha(new_id[i]) || isdigit(new_id[i]) || 
+            new_id[i] == '-' || new_id[i] == '_' || new_id[i] == '.'){
+                if(temp == '.' && new_id[i] == '.')
+                continue;
+                
+                answer+=new_id[i];
+            }
+            temp = new_id[i];
+        }
+        answer = ifempty(answer);
+        //step 4
+        if(answer[0] == '.'){
+            answer.erase(answer.begin());
+        }
+        int size = answer.size()-1;
+        if(answer[size] == '.'){
+            answer.erase(answer.end()-1);
+        }
+        answer = ifempty(answer);
+        //step5
+        if(answer.size() > 15){
+            answer.erase(15, answer.length());
+        }
+        answer = ifempty(answer);
 
-cout << answer << endl<<endl;
-    return answer;
-}
+        if(answer[0] == '.'){
+            answer.erase(answer.begin());
+        }
+        size = answer.size()-1;
+        if(answer[size] == '.'){
+            answer.erase(answer.end()-1);
+        }
+
+        if(answer.size() < 3)
+        while(answer.size() < 3){
+            answer+=answer.back();
+        }
+
+
+
+    cout << answer << endl;
+        return answer;
+    }
 
 
 int main () {
