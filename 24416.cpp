3d2ae2c4a1ac5c865d909fd9eb4eda7c2c0c0675
Minseok Kim <kim970602@gmail.com>
2022-06-07 04:56:59 +0900

    dp

---
+++ 24416.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int f[41];
+int fib1(int n,int count1){
+    if(n == 1 || n == 2) return 1;
+    return fib1(n-1,count1+1) + fib1(n-2,count1+1);
+}
+
+int fib2(int n, int count2){
+    f[0] = 1;
+    f[1] = 1;
+    for(int i = 3; i < n; i++){
+        f[i] = f[n-1] + f[n-2];
+        count2++;
+    }
+    return count2;
+}
+
+int main () {
+    int n;
+    int count1 = 0;
+    int count2 = 1;
+    cin >> n;
+    int a = fib1(n,count1);
+    cout << a << " ";
+    int b = fib2(n,count2);
+    cout << b;
+}
--- bestSum.cpp
+++ bestSum.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 string combination = "";
 
-string bestSum(int targetNum,vector<int>& numbers){
+string bestSum(int targetNum,vector<int>& numbers,unordered_map<int,string> memo){
+    if(memo.find(targetNum) != memo.end()) return memo[targetNum];
     if(targetNum == 0) return "";
     if(targetNum < 0) return "-1";
 
@@ -14,7 +15,7 @@ string bestSum(int targetNum,vector<int>& numbers){
 
     for(int i = 0; i <numbers.size(); i++){
         int remainder = targetNum-numbers[i];
-        string remainderCombination = bestSum(remainder,numbers);
+        string remainderCombination = bestSum(remainder,numbers,memo);
         if(remainderCombination != "-1"){
             combination +=to_string(numbers[i]);
             if(shorestCombination == "" || combination.size() < shorestCombination.size()){
@@ -22,16 +23,16 @@ string bestSum(int targetNum,vector<int>& numbers){
             }
         }
     }
-    return shorestCombination;
+    memo[targetNum] = shorestCombination;
+    return shorestCombination; 
 
 }
 
 int main () {
-vector<int> numbers;
-for(int i  = 0; i < 3; i++){
-    numbers.push_back(i+1);
-}
-unordered_map<int,bool> memo;
-string result = bestSum(8,numbers);
-cout << result;
+vector<int> numbers = {2,3,5};
+unordered_map<int,string> memo;
+string result = bestSum(8,numbers,memo);
+cout << result << endl;
+result = bestSum(10,numbers,memo);
+cout << result << endl;
 }
