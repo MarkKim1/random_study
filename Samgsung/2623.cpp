fb2425dc4568b5f6d60e47e588537bf330112101
Minseok Kim <kim970602@gmail.com>
2023-03-07 23:31:34 +0900

    class 5

---
+++ Samgsung/2623.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    vector<int> vec(N + 1);
+    for (int i = 0; i < M; i++)
+    {
+        }
+}
