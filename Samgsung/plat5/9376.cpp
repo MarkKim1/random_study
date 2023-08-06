f1eaaa077226c645d0a1aa484bda84dcd27f1447
Minseok Kim <kim970602@gmail.com>
2023-08-05 23:34:32 -0500

    plat 4

--- Samgsung/plat5/9376.cpp
+++ Samgsung/plat5/9376.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 #include <string.h>
+#include <string>
 using namespace std;
 
-int prison[3][101][101];
-vector<vector<char>> map;
-vector<pair<int, int>> prisoner;
+char prison[102][102];
+int min_door[3][102][102];
+int height, width;
+pair<int, int> prisoner[2];
 vector<int> dx = {0, 1, 0, -1};
 vector<int> dy = {1, 0, -1, 0};
-int R, C;
-struct INFO
-{
-    int r, c, count;
-    bool operator<(const INFO &p2) const
-    {
-        return count > p2.count;
-    }
-};
 
 void solve(int person_index, int r, int c)
 {
-    priority_queue<INFO> pq;
-    prison[person_index][r][c] = 0;
-    pq.push({r, c, 0});
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+    min_door[person_index][r][c] = 0;
+    pq.push({0, {r, c}});
 
     while (!pq.empty())
     {
-        int curr_r = pq.top().r;
-        int curr_c = pq.top().c;
-        int curr_count = pq.top().count;
+        int curr_r = pq.top().second.first;
+        int curr_c = pq.top().second.second;
+        int curr_count = pq.top().first;
         pq.pop();
-        if (curr_count > prison[person_index][curr_r][curr_c])
+        if (curr_count > min_door[person_index][curr_r][curr_c])
         {
             continue;
         }
@@ -40,20 +33,20 @@ void solve(int person_index, int r, int c)
             int next_r = curr_r + dx[i];
             int next_c = curr_c + dy[i];
             int next_count = curr_count;
-            if (min(next_r, next_c) >= 0 and next_r <= R and next_c <= C)
+            if (min(next_r, next_c) >= 0 and next_r < height + 2 and next_c < width + 2)
             {
-                if (map[next_r][next_c] == '*')
+                if (prison[next_r][next_c] == '*')
                 {
                     continue;
                 }
-                if (map[next_r][next_c] == '#')
+                if (prison[next_r][next_c] == '#')
                 {
                     next_count++;
                 }
-                if (next_count < prison[person_index][next_r][next_c])
+                if (next_count < min_door[person_index][next_r][next_c])
                 {
-                    prison[person_index][next_r][next_c] = next_count;
-                    pq.push({next_r, next_c, next_count});
+                    min_door[person_index][next_r][next_c] = next_count;
+                    pq.push({next_count, {next_r, next_c}});
                 }
             }
         }
@@ -62,45 +55,57 @@ void solve(int person_index, int r, int c)
 
 int main()
 {
-    int T;
-    cin >> T;
-    while (T--)
+    ios_base::sync_with_stdio(0);
+    int test_num;
+    cin >> test_num;
+
+    while (test_num--)
     {
-        cin >> R >> C;
-        map.resize(R + 2, vector<char>(C + 2, '.'));
-        for (int i = 1; i < R + 1; i++)
+        cin >> height >> width;
+
+        int order = 0;
+        string st;
+        for (int r = 1; r <= height; ++r)
         {
-            for (int j = 1; j < C + 1; j++)
+            cin >> st;
+            st = '.' + st + '.';
+            for (int c = 0; c <= width + 1; ++c)
             {
-                cin >> map[i][j];
-                if (map[i][j] == '$')
-                {
-                    prisoner.push_back({i, j});
-                }
+                prison[r][c] = st[c];
+                if (prison[r][c] == '$')
+                    prisoner[order++] = {r, c};
             }
         }
-        memset(prison, 0xf, sizeof(prison));
+
+        for (int c = 0; c <= width + 1; ++c)
+        {
+            prison[0][c] = '.';
+            prison[height + 1][c] = '.';
+        }
+
+        memset(min_door, 0xf, sizeof(min_door)); // 0xf로 초기화 해주자.
         solve(0, 0, 0);
         solve(1, prisoner[0].first, prisoner[0].second);
         solve(2, prisoner[1].first, prisoner[1].second);
-        int ans = INT_MAX;
-        for (int i = 0; i <= R + 1; i++)
+
+        int min_open_num = INT32_MAX;
+        int opened_doors;
+        for (int r = 0; r <= height + 1; ++r)
         {
-            for (int j = 0; j <= C + 1; j++)
+            for (int c = 0; c <= width + 1; ++c)
             {
-                if (map[i][j] == '*')
-                {
+                if (prison[r][c] == '*')
                     continue;
-                }
-                int curr_min = prison[0][i][j] + prison[1][i][j] + prison[2][i][j];
-                if (map[i][j] == '#')
-                {
-                    curr_min -= 2;
-                }
-                ans = min(ans, curr_min);
+
+                opened_doors = min_door[0][r][c] + min_door[1][r][c] + min_door[2][r][c];
+                if (prison[r][c] == '#') // 해당 좌표가 문인 경우
+                    opened_doors -= 2;
+                min_open_num = min(opened_doors, min_open_num);
             }
         }
-        prisoner.clear();
-        cout << "ans: " << ans << endl;
+
+        cout << min_open_num << '\n';
     }
+
+    return 0;
 }
