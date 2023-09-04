24de30cc96a3335a074ffa0d4f4187120152aa96
Minseok Kim <kim970602@gmail.com>
2023-09-04 02:10:36 -0500

    gold 1

--- Samgsung/2098.cpp
+++ Samgsung/2098.cpp
@@ -1,53 +1,52 @@
 #include <iostream>
 #include <cstring>
 
+#define endl "\n"
+#define MAX 16
+#define INF 987654321
 using namespace std;
 
-#define INF 987654321;
+int N, Answer_Bit;
+int MAP[MAX][MAX];
+int Cost[MAX][1 << MAX];
 
-int n, map[16][16];
-int dp[16][1 << 16];
-
-int dfs(int curr, int visit)
+int solve(int Cur_Node, int Cur_Bit)
 {
-    if (visit == (1 << n) - 1)
+    if (Cur_Bit == Answer_Bit)
     {
-        if (map[curr][0] == 0)
-        {
+        if (MAP[Cur_Node][0] == 0)
             return INF;
-        }
-        return map[curr][0];
+        else
+            return MAP[Cur_Node][0];
     }
-    if (dp[curr][visit] != -1)
+
+    if (Cost[Cur_Node][Cur_Bit] != -1)
+        return Cost[Cur_Node][Cur_Bit];
+    Cost[Cur_Node][Cur_Bit] = INF;
+
+    for (int i = 0; i < N; i++)
     {
-        return dp[curr][visit];
-    }
-    dp[curr][visit] = INF;
-    for (int i = 0; i < n; i++)
-    {
-        if (map[curr][i] == 0)
-        {
+        if (MAP[Cur_Node][i] == 0)
             continue;
-        }
-        if ((visit & (1 << i)) == (1 << i)) // 이미 방문 했다면 다음으로 넘어간다.
-        {
+        if ((Cur_Bit & (1 << i)) == (1 << i))
             continue;
-        }
-        dp[curr][visit] = min(dp[curr][visit], map[curr][i] + dfs(i, visit | 1 << i));
+        Cost[Cur_Node][Cur_Bit] = min(Cost[Cur_Node][Cur_Bit], MAP[Cur_Node][i] + solve(i, Cur_Bit | 1 << i));
     }
-    return dp[curr][visit];
+    return Cost[Cur_Node][Cur_Bit];
 }
 
 int main()
 {
-    cin >> n;
-    for (int i = 0; i < n; i++)
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < N; j++)
         {
-            cin >> map[i][j];
+            cin >> MAP[i][j];
         }
     }
-    memset(dp, -1, sizeof(dp));
-    cout << dfs(0, 1) << "\n";
+    Answer_Bit = (1 << N) - 1;
+    memset(Cost, -1, sizeof(Cost));
+    cout << solve(0, 1) << endl;
 }
