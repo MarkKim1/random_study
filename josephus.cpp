d5016c627747bf446cbf7fa8bdb8b22151be49dc
Minseok Kim <kim970602@gmail.com>
2023-10-29 22:47:25 -0500

    Update josephus.cpp

--- josephus.cpp
+++ josephus.cpp
@@ -4,23 +4,28 @@
 #include <string>
 using namespace std;
 
-int main () {
+int main()
+{
     int members;
     int permutation;
     queue<int> que;
     cin >> members;
     cin >> permutation;
-    for(int i = 1; i <=members; i++){
+    for (int i = 1; i <= members; i++)
+    {
         que.push(i);
     }
     cout << "<";
-    while(que.size()){
-        if(que.size() == 1){
+    while (que.size())
+    {
+        if (que.size() == 1)
+        {
             cout << que.front() << ">";
             que.pop();
             break;
         }
-        for(int i = 1; i < permutation; i++){
+        for (int i = 1; i < permutation; i++)
+        {
             que.push(que.front());
             que.pop();
         }
